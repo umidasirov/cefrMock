ee3eaa3ee0c7b50b084435e9c5cdbec3648e2105
umidasirov <126274140+umidasirov@users.noreply.github.com>
2025-10-27 12:49:10 +0500

    salom dunyo

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import App from './components/app/app'
+import { StrictMode } from 'react'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
