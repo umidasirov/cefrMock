7ce776e9d7f24b9c36bed097e3f5558c08bb6dbd
umidasirov <126274140+umidasirov@users.noreply.github.com>
2025-10-29 08:55:16 +0300

    Update main.jsx

--- src/components/bodylar/main/main.jsx
+++ src/components/bodylar/main/main.jsx
@@ -1,5 +1,5 @@
 import Baneer from "./components/banner/Baneer";
-import CEFRLevels from "./components/CEFRLevels/CEFRLevels";
+import CEFRLevels from "./components/cefrLevels/CEFRLevels";
 import Features from "./components/features/features";
 
 export default function Main() {
